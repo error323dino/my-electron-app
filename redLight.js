dc340911f001c920d4441468f535cac88324b84a
WTHUW <75290505+error323dino@users.noreply.github.com>
2023-09-26 00:00:49 +0800

    ok done i guess

--- redLight.js
+++ redLight.js
@@ -76,22 +76,29 @@ function findChildPath(location) {
   });
 }
 
+// Function to display the screenshots without sorting
 function displayScreenshots(location, snapshot) {
   const ssContainer = document.getElementById('listScreenshot');
   ssContainer.innerHTML = '';
 
   const objects = snapshot.val();
+  let recordsFound = false; 
 
   let row = document.createElement('div');
   row.classList.add('grid-row'); // Create a row container
 
   let count = 0; // To keep track of items in the current row
 
-  // Iterate through the objects and display only those matching the selected location
+  // Create an array to collect the items to display
+  const screenshotItems = [];
+
+  // Iterate through the objects and collect only those matching the selected location
   for (const objectName in objects) {
     if (objects.hasOwnProperty(objectName)) {
       const objectDetails = objects[objectName];
       if (objectDetails.Location === location) {
+        recordsFound = true; 
+        console.log("ok")
         const objectDiv = document.createElement('div');
         objectDiv.classList.add('grid-item'); // Assuming you have a CSS class for styling grid items
 
@@ -125,58 +132,79 @@ function displayScreenshots(location, snapshot) {
 
         // If we have reached two items in the current row, start a new row
         if (count === 2) {
-          ssContainer.appendChild(row);
-          row = document.createElement('div');
-          row.classList.add('grid-row'); // Create a new row container
+          screenshotItems.push(row.cloneNode(true)); // Clone the row and add it to the array
+          row.innerHTML = ''; // Clear the row for the next items
           count = 0;
         }
       }
     }
   }
-
-  // Append the last row if it's not full
-  if (count > 0) {
-    ssContainer.appendChild(row);
+  if (!recordsFound) {
+    const noRecordsDiv = document.createElement('div');
+    noRecordsDiv.textContent = 'No record found for this location.';
+    noRecordsDiv.classList.add('no-records-message'); // Add the CSS class
+    ssContainer.appendChild(noRecordsDiv);
   }
+  
+  if(count > 0) {
+    screenshotItems.push(row); // Add the last row if it's not empty
+  }
+
+  // Log the screenshot items to the console
+  console.log(screenshotItems);
+
+  // Add the screenshot items to the container
+  screenshotItems.forEach((item) => {
+    ssContainer.appendChild(item);
+  });
 }
 
+// Function to extract timestamp
 function extractTimestamp(textContent) {
-  const timestampString = textContent.match(/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}/);
-  if (timestampString) {
+  const timestampRegex = /(\d{2}\/\d{2} \d{2}:\d{2}:\d{2})/g;
+  const timestamps = textContent.match(timestampRegex);
+
+  if (timestamps && timestamps.length > 0) {
+    // Use the first timestamp found in the text content for sorting
+    const timestampString = timestamps[0];
+    const [month, day, time] = timestampString.split(" ");
     const currentYear = new Date().getFullYear(); // Get the current year
     // Append the current year to the timestamp string to make it 'YYYY/MM/DD HH:MM:SS'
-    const timestampWithYear = `${currentYear}/${timestampString[0]}`;
+    const timestampWithYear = `${currentYear}/${timestampString}`;
     return new Date(timestampWithYear);
   }
+
   // Return a default date in case of missing or invalid timestamps
   return new Date(0);
 }
 
+
 // Track the current sorting order
 let ascendingOrder = true;
 
-// Select the SVG button by its id
+// Select the sorting button by its ID
 const sortButton = document.getElementById('sortButton');
 
 // Add a click event listener to the button
 sortButton.addEventListener('click', () => {
   // Toggle the sorting order
   ascendingOrder = !ascendingOrder;
-  
+
   // Call the function to sort the output
   sortOutput();
-  
+
   // Update the sort icon based on the sorting order
   updateSortIcon();
 });
 
+// Function to sort the output
 function sortOutput() {
   const listScreenshot = document.getElementById('listScreenshot');
-  const screenshotItems = Array.from(listScreenshot.children);
+  const screenshotItems = Array.from(listScreenshot.querySelectorAll('.grid-item'));
 
   screenshotItems.sort((a, b) => {
-    const timestampA = extractTimestamp(a.textContent);
-    const timestampB = extractTimestamp(b.textContent);
+    const timestampA = extractTimestamp(a.innerHTML);
+    const timestampB = extractTimestamp(b.innerHTML);
 
     // Sort in ascending or descending order based on the current sorting order
     if (ascendingOrder) {
@@ -187,12 +215,26 @@ function sortOutput() {
   });
 
   // Clear the container and append the sorted elements
-  listScreenshot.innerHTML = '';
-  screenshotItems.forEach((item) => {
-    listScreenshot.appendChild(item);
+  listScreenshot.innerHTML = ''; // Clear the existing content
+
+  const columnsPerRow = 2; // Adjust this number to change the number of columns per row
+  let currentRow;
+  screenshotItems.forEach((item, index) => {
+    if (index % columnsPerRow === 0) {
+      // Start a new row
+      currentRow = document.createElement('div');
+      currentRow.classList.add('grid-row');
+    }
+    currentRow.appendChild(item);
+    if ((index + 1) % columnsPerRow === 0 || index === screenshotItems.length - 1) {
+      // Append the row to the container when it's full or for the last item
+      listScreenshot.appendChild(currentRow);
+    }
   });
 }
 
+
+// Function to update the sort icon
 function updateSortIcon() {
   const sortIcon = document.querySelector('#sortButton svg');
   if (ascendingOrder) {
@@ -208,7 +250,7 @@ function updateSortIcon() {
 
 // Initial sorting
 sortOutput();
-updateSortIcon();
+
 document.getElementById('goBackLink').addEventListener('click', function (event) {
   event.preventDefault(); // Prevent the default link behavior
   window.history.back(); // Navigate back to the previous page
